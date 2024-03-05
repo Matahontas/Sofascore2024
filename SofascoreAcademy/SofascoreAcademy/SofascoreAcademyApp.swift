9d18d9a12f9cdde6ef22e1b104e1b001ea43587a
Matahontas <mata.pavicic@gmail.com>
2024-03-05 17:20:57 +0100

    Created a new project

---
+++ SofascoreAcademy/SofascoreAcademy/SofascoreAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SofascoreAcademyApp.swift
+//  SofascoreAcademy
+//
+//  Created by Matija Pavicic on 05.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct SofascoreAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
